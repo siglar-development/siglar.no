c7ce12bd844ae147fb5eee1444b9093e73597f19
Vue Play <author@vueplay.com>
2024-06-13 13:05:47 +0200

    Stored by Vue Play

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -21,7 +21,7 @@
 				class="container"
 			>
 				<p class="mt-8 mb-2">
-					Ta kontakt for en hyggelig prat med oss.
+					Ta kontakt for en hyggelig prat.
 				</p>
 				<address class="not-italic mb-12">
 					<label for="">
